d21705810dfed3aa0a2b78628e94114a2124c0e6
galaxy4276 <galaxyhi4276@gmail.com>
2023-09-20 06:20:13 +0900

    feat: 헤더 정보를 HashMap 으로 관리하는 객체 작성

---
+++ src/main/java/org/euncat/socket/HttpRequest.java
@@ -0,0 +1,31 @@
+package org.euncat.socket;
+
+import org.euncat.dto.SimpleHttpRequest;
+
+import java.util.HashMap;
+
+public class HttpRequest {
+    private HashMap<String, String> headers = new HashMap<>();
+    private SimpleHttpRequest simpleHttpRequest;
+
+    public HttpRequest(SimpleHttpRequest request) {
+        this.simpleHttpRequest = request;
+    }
+
+    public void setHeader(String key, String value) {
+        headers.put(key, value);
+    }
+
+    public String getHeader(String key) {
+        return headers.get(key);
+    }
+
+    public String getRequestUrl() {
+        return simpleHttpRequest.getRequestUrl();
+    }
+
+    public String getMethod() {
+        return simpleHttpRequest.getMethod();
+    }
+
+}
